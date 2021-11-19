d87255bf3d9a61ea2795454ddfa24c45489e193d
Greg Landry <greg.landry@infineon.com>
2021-11-19 10:48:20 -0500

    Remove exercies that use obsolete library

--- Projects/key_ch05a_ex12_custom_bsp/TARGET_MYKIT/docs/html/modules.js
+++
@@ -1,7 +0,0 @@
-var modules =
-[
-    [ "Macros", "group__group__bsp__macros.html", "group__group__bsp__macros" ],
-    [ "Functions", "group__group__bsp__functions.html", "group__group__bsp__functions" ],
-    [ "Pin States", "group__group__bsp__pin__state.html", "group__group__bsp__pin__state" ],
-    [ "Pin Mappings", "group__group__bsp__pins.html", "group__group__bsp__pins" ]
-];
